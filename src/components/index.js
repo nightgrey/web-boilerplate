3051b01bdceae5c0dbb4ba5e36334ff63a9bd994
Nico <nico@nachtgrau.org>
2016-04-02 20:09:18 +0200

    Minor changes, added missing dependencies

--- src/components/index.js
+++ src/components/index.js
@@ -2,6 +2,6 @@
 import app from './app';
 import footer from './footer';
 import header from './header';
-/* eslint-disable no-unused-vars */
 
+/* eslint-disable no-console */
 console.log('Hello, web-boilerplate.');
